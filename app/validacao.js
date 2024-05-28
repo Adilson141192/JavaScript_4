5236e549ae693734fa2302deb81f6f5cae86fb44
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2024-05-28 13:48:27 -0300

    052824 aula0404

--- app/validacao.js
+++ app/validacao.js
@@ -3,12 +3,14 @@ function verificaSeOChutePossuiUmValorValido(chute) {
 
     if (chuteForInvalido(numero)) {
         elementoChute.innerHTML += '<div>Valor inválido!</div>'
+        return
     }
 
     if (numeroForMaiorOuMenorQueOValorPermitido) {
         elementoChute.innerHTML += `
         <div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}.</div>
         `
+        return
     }
 
     if (numero === numeroSecreto) {
@@ -16,6 +18,10 @@ function verificaSeOChutePossuiUmValorValido(chute) {
         <h2>Você Acertou!</h2>
         <h3>O número secreto era ${numeroSecreto}!</h3>
         `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>`
+    } else {
+        elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>`
     }
 
 }
